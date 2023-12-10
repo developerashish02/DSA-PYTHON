46a5fd04b771df35f2b310fbacbab7db8ea59cc4
Ashish Gaikwad <developerashish02@gmail.com>
2023-12-10 18:31:03 +0530

    lean merge sort in place

--- text.py
+++ text.py
@@ -1,5 +1,37 @@
+from typing import List
 
 
-nums = [-100, -98, 1, 2, 3, 4]
-nums.sort(reverse=True)
-print(nums)
+def maximumProduct(self, nums: List[int]) -> int:
+    # first three max
+    firstMax = float("-inf")
+    secondMax = float("-inf")
+    thirdMax = float("-inf")
+    # first two min
+    firstMin = float("inf")
+    secondMin = float("inf")
+
+    for item in nums:
+        # Find out the first max
+        if item >= firstMax:
+            thirdMax = secondMax
+            secondMax = firstMax
+            firstMax = item
+
+        # Find out the second max
+        elif item >= secondMax and item < firstMax:
+            thirdMax = secondMax
+            secondMax = item
+
+        elif item >= thirdMax and item < secondMax and item < firstMax:
+            thirdMax = item
+
+        # Find out the first min
+        if item <= firstMin:
+            secondMin = firstMin
+            firstMin = item
+
+        # Find out the second min
+        elif item <= secondMin and item < firstMin:
+            secondMin = item
+
+    return max((firstMax * secondMax * thirdMax), (firstMin * secondMin * firstMax))
