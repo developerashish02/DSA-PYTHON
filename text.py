7a0b72d4ef2e21ad9495bbde3e0bdd682dc80e68
Ashish Gaikwad <developerashish02@gmail.com>
2023-12-10 10:49:04 +0530

    Add merge sort code

--- text.py
+++ text.py
@@ -1,7 +1,5 @@
 
 
-nums = [1, 24, 3, 6, 8, 8, 8]
-mid = len(nums) // 2
-
-print(nums[:mid])
-# print("The last index of the target is : ", ans)
+nums = [-100, -98, 1, 2, 3, 4]
+nums.sort(reverse=True)
+print(nums)
